ec0544008acf5740d4540a127cfbd4aff88a6cc8
Elder Baltazar <Elder Baltazar>
2017-01-11 00:56:41 -0300

    implementing created verification

--- app/src/main/java/br/com/monitoratec/app/MainActivity.java
+++ app/src/main/java/br/com/monitoratec/app/MainActivity.java
@@ -1,11 +1,14 @@
 package br.com.monitoratec.app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ import java.io.IOException;
 
 import br.com.monitoratec.app.domain.GitHubStatusApi;
 import br.com.monitoratec.app.domain.entity.Status;
+import br.com.monitoratec.app.util.Util;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtStatus;
     private ImageView mImgVectorial;
+    private TextInputLayout usernameWrapper;
+    private TextInputLayout passwordWrapper;
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -43,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
         // binding
         txtStatus = (TextView) this.findViewById(R.id.txtStatus);
         mImgVectorial = (ImageView) this.findViewById(R.id.imgVector);
-
+        usernameWrapper = (TextInputLayout) findViewById(R.id.tilUsername);
+        passwordWrapper = (TextInputLayout) findViewById(R.id.tilPassword);
         // setting date format
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").create();
 
@@ -62,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     Status status = response.body();
                     updateScreen(status.type.getColorId(),response.body().body);
-                    Toast.makeText(MainActivity.this, status.type.name(), Toast.LENGTH_LONG).show();
                 } else {
 
                     try {
@@ -90,7 +96,29 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void btnLoginClicked(View view) {
+        hideKeyboard();
 
+        String username = usernameWrapper.getEditText().getText().toString();
+        String password = usernameWrapper.getEditText().getText().toString();
+        if (!Util.validateEmail(username)) {
+            usernameWrapper.setError("*Not a valid email address!");
+        } else if (!Util.validatePassword(password)) {
+            passwordWrapper.setError("Not a valid password!");
+        } else {
+            usernameWrapper.setErrorEnabled(false);
+            passwordWrapper.setErrorEnabled(false);
+            doLogin();
+        }    
+    }
 
+    private void doLogin() {
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
